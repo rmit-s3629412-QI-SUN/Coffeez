885960c56f2e4c55ee5102e3edfd07896de9083e
rmit-s3629412-QI-SUN <42260597+rmit-s3629412-QI-SUN@users.noreply.github.com>
2019-05-27 10:12:37 +1000

    Add files via upload

--- cart.php
+++ cart.php
@@ -17,17 +17,65 @@ if (mysqli_num_rows($result) > 0) {
     $espPrice = $row['esp']*5;
     $sandPrice = $row['sandwich']*5;
     $sum = $espPrice + $sandPrice;
-    echo"<p>the Espose number is: ".$row['esp']."</p>";
-    echo"<p>the Espose size is: small</p>";
-    echo"<p>the Espose price is: ".$espPrice."</p>";
-    echo"<p>the sandwich number is: ".$row['sandwich']."</p>";
-    echo"<p> the sandwich price is: ".$sandPrice."</p>";
-    echo"<p> the total price price is: ".$sum."</p>";
 
-    $showSandwichNumber = $row['sandwich'];
-    $showEspNumber = $row['esp'];
-    
-    }
+    echo "
+    <table align='center'>
+        <tr>
+            <td>
+                <div> The Espresso number is :</div>
+            </td>
+            <td>
+            ".$row['esp']."
+            </td>
+        </tr>
+        <tr>
+            <td>
+                The Espresso size is:
+            </td>
+            <td>
+                small
+            </td>
+        </tr>
+        <tr>
+            <td>
+                The Espresso price is:
+            </td>
+             <td>
+             ".$espPrice."
+             </td>
+        </tr>
+        <tr>
+            <td>
+                The sandwich number is:
+            </td>
+            <td>
+            ".$row['sandwich']."
+            </td>
+        </tr>
+        <tr>
+            <td>
+                The sandwich price is:
+            </td>
+            <td>
+            ".$sandPrice."
+            </td>
+        </tr>
+        <tr>
+            <td>
+                The total price is:
+            </td>
+            <td>
+            ".$sum."
+            </td>
+        </tr>
+    </table>
+        <input id='pay' type='button' value='pay' name='pay'>
+
+         <script>
+            var pay = document.getElementById('pay');
+            pay.onclick = window.location.href = 'payment.html'
+        </script>
+    ";}
 } else {
     echo "0 results";
 }
--- staff_order.php
+++ staff_order.php
@@ -13,20 +13,46 @@ $result = mysqli_query($con,$sql);
 
 while($row = $result->fetch_assoc())
 {
-    $espPrice = $row['esp']*5;
-    $sandPrice = $row['sandwich']*5;
-    $sum = $espPrice + $sandPrice;
-    echo"<p>the Espose number is: ".$row['esp']."</p>";
-    echo"<p>the Espose size is: small</p>";
-    
-    echo"<p>the sandwich number is: ".$row['sandwich']."</p>";
-    echo"<input id='changeStatus' type='button' value='complete' name='changeStatus'>
-        <script>
-            var change = document.getElementById('changeStatus')
-            change.onclick = function () {
-                window.location.href = 'complete.html'
-            }
 
-        </script>";
+    echo " 
+    <table align = 'center' >
+    <tr>
+        <td>
+            <div>The Espresso number is : </div>
+        </td>
+        <td>
+                ".$row['esp']."
+        </td>
+    </tr>
+    <tr>
+        <td>
+            The Espresso size is:
+        </td>
+        <td>
+            small
+        </td>
+    </tr>
+    <tr>
+        <td>
+            The sandwich number is:
+        </td>
+        <td>
+                ".$row['sandwich']."
+        </td>
+    </tr>
+    <tr>
+        <td colspan='2'>
+            <input id='changeStatus' type='button' value='complete' name='change'>
+            <script>
+                var change = document.getElementById('changeStatus')
+                change.onclick = function () {
+                    window.location.href = 'complete.html'
+                }
+
+            </script>
+        </td>
+    </tr>
+</table>
+    ";
 }
 ?>
